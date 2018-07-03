d973ee096fd4da836e71f2929f1e3fb2efe91875
Dominik Moser <dominik.moser@erstegroup.com>
2018-07-03 21:09:21 +0200

    add oracle

--- casino-oracle/src/main/kotlin/at/logic/sc/ss18/casino/casinooracle/CasinoOracleApplication.kt
+++ casino-oracle/src/main/kotlin/at/logic/sc/ss18/casino/casinooracle/CasinoOracleApplication.kt
@@ -1,12 +1,21 @@
 package at.logic.sc.ss18.casino.casinooracle
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.methods.request.EthFilter
 import org.web3j.protocol.http.HttpService
 import org.web3j.tx.ClientTransactionManager
 import org.web3j.tx.TransactionManager
@@ -14,20 +23,82 @@ import org.web3j.tx.gas.ContractGasProvider
 import org.web3j.tx.gas.DefaultGasProvider
 import java.math.BigInteger
 import java.time.ZonedDateTime
+import java.util.PriorityQueue
 
 fun main(args: Array<String>) {
     runApplication<CasinoOracleApplication>(*args)
 }
 
 @SpringBootApplication
+@EnableScheduling
+@EnableFeignClients
 class CasinoOracleApplication(
-        val web3j: Web3j,
-        val casino: Casino
+        val casino: Casino,
+        val etherPriceService: EtherPriceService,
+        val cryptoPriceService: CryptoPriceService
 ) : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        println(web3j.web3ClientVersion().send())
-        println(casino.setInformation(ZonedDateTime.now().toEpochSecond().BI, 100.BI).send())
+    override fun run(vararg args: String?) =
+            casino.observeBetPlacedEvents(EthFilter()).forEach { betPlacedEvent ->
+                with(betPlacedEvent) {
+                    etherPriceService.reportPriceToOracle(betPlacedTimestamp.toLong())
+                    etherPriceService.reportPriceToOracle(betTimestamp.toLong())
+                }
+            }
+}
+
+@Service
+class EtherPriceService(
+        val casino: Casino,
+        val cryptoPriceService: CryptoPriceService
+) {
+
+    val futureTimestamps = PriorityQueue<Long>()
+
+    fun reportPriceToOracle(timestamp: Long) {
+        if (isInPast(timestamp)) {
+            publishPrice(timestamp, resolvePrice(timestamp))
+        } else {
+            futureTimestamps.add(timestamp)
+        }
     }
+
+    private fun isInPast(timestamp: Long) = timestamp <= ZonedDateTime.now().toEpochSecond()
+
+    @Scheduled(fixedRate = 30000)
+    fun continuouslyResolvePrices() =
+            futureTimestamps
+                    .takeWhile { isInPast(it) }
+                    .forEach { publishPrice(it, resolvePrice(it)) }
+
+    private fun publishPrice(timestamp: Long, price: Long) {
+        casino.setInformation(timestamp.BI, price.BI).send()
+    }
+
+    private fun resolvePrice(timestamp: Long): Long =
+            cryptoPriceService.retrievePrice(
+                    fromSym = "ETH",
+                    toSym = "EUR",
+                    limit = 1,
+                    timestamp = timestamp
+            ).priceItems.last().price.times(100).toLong()
+
+}
+
+
+@FeignClient(name = "cryptocompare", url = "https://min-api.cryptocompare.com")
+interface CryptoPriceService {
+
+    @GetMapping("/data/histominute")
+    fun retrievePrice(
+            @RequestParam("fsym") fromSym: String,
+            @RequestParam("tsym") toSym: String,
+            @RequestParam("toTs") timestamp: Long,
+            @RequestParam("limit") limit: Long
+    ): CryproPriceResponse
+
+    data class CryproPriceResponse(@JsonProperty("Data") val priceItems: List<PriceItem>)
+    data class PriceItem(@JsonProperty("time") val timestamp: Long, @JsonProperty("close") val price: Double)
+
 }
 
 @Configuration
@@ -65,6 +136,3 @@ class Web3jConfigurationProperties {
 
 private val Long.BI: BigInteger
     get() = BigInteger.valueOf(this)
-
-private val Int.BI: BigInteger
-    get() = BigInteger.valueOf(this.toLong())
