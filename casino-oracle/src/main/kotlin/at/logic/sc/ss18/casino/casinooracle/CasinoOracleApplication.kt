6cbaf048839e42cf6f69a1516da4141001ab5c65
Dominik Moser <dominik.moser@erstegroup.com>
2018-07-03 19:05:42 +0200

    Add first version of casino

--- casino-oracle/src/main/kotlin/at/logic/sc/ss18/casino/casinooracle/CasinoOracleApplication.kt
+++ casino-oracle/src/main/kotlin/at/logic/sc/ss18/casino/casinooracle/CasinoOracleApplication.kt
@@ -8,15 +8,25 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.http.HttpService
+import org.web3j.tx.ClientTransactionManager
+import org.web3j.tx.TransactionManager
+import org.web3j.tx.gas.ContractGasProvider
+import org.web3j.tx.gas.DefaultGasProvider
+import java.math.BigInteger
+import java.time.ZonedDateTime
 
 fun main(args: Array<String>) {
     runApplication<CasinoOracleApplication>(*args)
 }
 
 @SpringBootApplication
-class CasinoOracleApplication(val web3j: Web3j) : CommandLineRunner {
+class CasinoOracleApplication(
+        val web3j: Web3j,
+        val casino: Casino
+) : CommandLineRunner {
     override fun run(vararg args: String?) {
         println(web3j.web3ClientVersion().send())
+        println(casino.setInformation(ZonedDateTime.now().toEpochSecond().BI, 100.BI).send())
     }
 }
 
@@ -24,7 +34,7 @@ class CasinoOracleApplication(val web3j: Web3j) : CommandLineRunner {
 @ConfigurationProperties(prefix = "casino")
 class CasinoConfigurationProperties {
 
-    lateinit var address : String
+    lateinit var address: String
 
 }
 
@@ -32,7 +42,16 @@ class CasinoConfigurationProperties {
 class Web3jConfiguration {
 
     @Bean(destroyMethod = "shutdown")
-    fun web3j(config : Web3jConfigurationProperties) = Web3j.build(HttpService(config.networkAddress))
+    fun web3j(config: Web3jConfigurationProperties) = Web3j.build(HttpService(config.networkAddress))
+
+    @Bean
+    fun gasProvider(): ContractGasProvider = DefaultGasProvider()
+
+    @Bean
+    fun transactionManager(web3j: Web3j, config: Web3jConfigurationProperties): TransactionManager =
+            ClientTransactionManager(web3j, config.accountAddress)
+
+
 }
 
 @Configuration
@@ -40,6 +59,12 @@ class Web3jConfiguration {
 class Web3jConfigurationProperties {
 
     var networkAddress = "http://localhost:8545/"
+    var accountAddress = ""
 
 }
 
+private val Long.BI: BigInteger
+    get() = BigInteger.valueOf(this)
+
+private val Int.BI: BigInteger
+    get() = BigInteger.valueOf(this.toLong())
