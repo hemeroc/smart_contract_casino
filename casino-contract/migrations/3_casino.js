2f13ad27c9edfe72f144a2c8f9130db763eb5a1f
Dominik Moser <dominik.moser@erstegroup.com>
2018-07-03 17:58:21 +0200

    set rational initial token value

--- casino-contract/migrations/3_casino.js
+++ casino-contract/migrations/3_casino.js
@@ -1,6 +1,6 @@
 var Casino = artifacts.require("./casino/Casino.sol");
 
 module.exports = function(deployer) {
-    let initialBeerTokenPrice = web3.toWei(1, 'ether');
+    let initialBeerTokenPrice = web3.toWei(0.1, "ether");
     deployer.deploy(Casino, initialBeerTokenPrice);
 };
